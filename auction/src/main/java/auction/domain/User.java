43978ea383e456b18adf6c4bd1b3131a78f98209
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-05-09 09:30:52 +0200

    2

--- auction/src/main/java/auction/domain/User.java
+++ auction/src/main/java/auction/domain/User.java
@@ -1,6 +1,14 @@
 package auction.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id @GeneratedValue
+    private long id;
 
     private String email;
 
@@ -9,7 +17,28 @@ public class User {
 
     }
 
+    public User() {
+
+    }
+
+    //region Getters & Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getEmail() {
         return email;
     }
+    //endregion
+
+
 }
