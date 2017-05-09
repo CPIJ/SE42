f8c98f46a170df917e29e8706d427b59a1ed6145
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-05-09 11:17:28 +0200

    Made unittests more readable

--- auction/src/test/java/auction/service/DefaultRegistrationServiceTest.java
+++ auction/src/test/java/auction/service/DefaultRegistrationServiceTest.java
@@ -24,26 +24,34 @@ public class DefaultRegistrationServiceTest {
     @Before
     public void setUp() throws Exception {
         defaultRegistrationService = new DefaultRegistrationService();
+
+        // Toegevoegd om te zorgen dat de database leeg is voordat iedere test wordt uitgevoerd.
         DatabaseCleaner cleaner = new DatabaseCleaner(entityManager);
         cleaner.clean();
     }
 
-    @Test
+    //region original
+    //@Test
     public void registerUser() {
+        // User mag geregistreerd worden, er is een @ aanwezig.
         User user1 = defaultRegistrationService.registerUser("xxx1@yyy");
         assertTrue(user1.getEmail().equals("xxx1@yyy"));
 
+        // User mag geregistreerd worden, er is een @ aanwezig.
         User user2 = defaultRegistrationService.registerUser("xxx2@yyy2");
         assertTrue(user2.getEmail().equals("xxx2@yyy2"));
 
+        // Emailadres bestaat al, de user met dit emailadres wordt opgehaald.
+        // Aangezien de entityManager nog niet herladen is zijn ook de geheugenadressen hetzelfde als
+        // ze opgehaald worden uit de db.
         User user2bis = defaultRegistrationService.registerUser("xxx2@yyy2");
         assertSame(user2bis, user2);
 
-//        geen @ in het adres
+        // Een emailadres zonder
         assertNull(defaultRegistrationService.registerUser("abc"));
     }
 
-    @Test
+    //@Test
     public void getUser() {
         User user1 = defaultRegistrationService.registerUser("xxx5@yyy5");
         User userGet = defaultRegistrationService.getUser("xxx5@yyy5");
@@ -56,7 +64,7 @@ public class DefaultRegistrationServiceTest {
         assertNull(defaultRegistrationService.getUser("abc"));
     }
 
-    @Test
+   // @Test
     public void getUsers() {
         List<User> users = defaultRegistrationService.getUsers();
         assertEquals(0, users.size());
@@ -74,5 +82,78 @@ public class DefaultRegistrationServiceTest {
         //geen nieuwe user toegevoegd, dus gedrag hetzelfde als hiervoor
         users = defaultRegistrationService.getUsers();
         assertEquals(2, users.size());
+    }
+    //endregion
+
+    @Test
+    public void RegisterUser_ValidEmailAddress_ReturnsUser() {
+        User user1 = defaultRegistrationService.registerUser("xxx1@yyy");
+        assertTrue(user1.getEmail().equals("xxx1@yyy"));
+    }
+
+    @Test
+    public void RegisterUser_SameEmailadressUsed_ReturnsSameObject() {
+        User user2 = defaultRegistrationService.registerUser("xxx2@yyy2");
+        assertTrue(user2.getEmail().equals("xxx2@yyy2"));
+
+        User user2bis = defaultRegistrationService.registerUser("xxx2@yyy2");
+        assertSame(user2bis, user2);
+    }
+
+    @Test
+    public void RegisterUser_InvalidEmailAddress_ReturnsNull() {
+        assertNull(defaultRegistrationService.registerUser("abc"));
+    }
+
+    @Test
+    public void GetUser_GivenSameEmail_ReturnsSameUser() {
+        User user1 = defaultRegistrationService.registerUser("xxx5@yyy5");
+        User userGet = defaultRegistrationService.getUser("xxx5@yyy5");
+
+        assertSame(userGet, user1);
+    }
+
+    @Test
+    public void GetUser_NonExistingEmail_ReturnsNull() {
+        assertNull(defaultRegistrationService.getUser("aaa4@bb5"));
+    }
+
+    @Test
+    public void GetUser_GivenInvalidEmail_ReturnsNull() {
+        defaultRegistrationService.registerUser("abc");
+        assertNull(defaultRegistrationService.getUser("abc"));
+    }
+
+    @Test
+    public void GetUsers_NoUsersInDatabase_ReturnsEmptyList() {
+        List<User> users = defaultRegistrationService.getUsers();
+        assertEquals(0, users.size());
+    }
+
+    @Test
+    public void GetUsers_OneUserInDatabase_ReturnsListWithSameUser() {
+        User user1 = defaultRegistrationService.registerUser("xxx8@yyy");
+        List<User> users = defaultRegistrationService.getUsers();
+        assertEquals(1, users.size());
+        assertSame(users.get(0), user1);
+    }
+
+    @Test
+    public void GetUsers_MultipleUsersInDatabase_ReturnsListWithUsers() {
+        User user1 = defaultRegistrationService.registerUser("xxx8@yyy");
+        List<User> users = defaultRegistrationService.getUsers();
+        assertEquals(1, users.size());
+        assertSame(users.get(0), user1);
+
+        User user2 = defaultRegistrationService.registerUser("xxx9@yyy");
+        users = defaultRegistrationService.getUsers();
+        assertEquals(2, users.size());
+    }
+
+    @Test
+    public void GetUsers_RegisterWithInvalidEmail_ReturnsEmptyList() {
+        defaultRegistrationService.registerUser("abc");
+        List<User> users = defaultRegistrationService.getUsers();
+        assertEquals(0, users.size());
     }
 }
